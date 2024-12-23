5b122b2f53c35095eff791af8d918c93bcbd3b86
Minginswe <xiaomingvlog@gmail.com>
2024-12-23 18:21:57 +0800

    import static resource

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+
+//引入初始化样式文件
+import '@/styles/common.scss'
 
 //测试接口函数
 import {getCategory} from '@/apis/testApi'
@@ -13,6 +14,8 @@ getCategory().then(res => {
 })
 
 const app = createApp(App)
+
+
 
 app.use(createPinia())
 app.use(router)
