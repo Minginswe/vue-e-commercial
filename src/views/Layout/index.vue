b37af13ceca9edf97a8ff965f4062ee56da08aa8
Minginswe <xiaomingvlog@gmail.com>
2024-12-25 14:43:03 +0800

    top fixed nav bar

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -2,12 +2,14 @@
 import LayoutNav from "./components/LayoutNav.vue"
 import LayoutHeader from "./components/LayoutHeader.vue"
 import LayoutFooter from "./components/LayoutFooter.vue"
+import LayoutFixed from "./components/LayoutFixed.vue"
 </script>
 
 
 <template>
     <div>
         <!-- 二级路由的出口 -->
+        <LayoutFixed /> 
         <LayoutNav />
         <LayoutHeader />
         <RouterView/>
