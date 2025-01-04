459374b5b21f68799e3da41e823bfad32bc3da7a
Minginswe <xiaomingvlog@gmail.com>
2025-01-04 16:17:57 +0800

    magnifier

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -21,8 +21,16 @@ const target = ref(null)
 const {elementX, elementY, isOutside} = useMouseInElement(target)
 const left = ref(0)
 const top = ref(0)
-watch([elementX, elementY], () => {
+
+const positionX = ref(0)
+const positionY = ref(0)
+
+watch([elementX, elementY, isOutside], () => {
     console.log('x,y变化了')
+    //如果鼠标没有移入到盒子里面 直接不执行后面的逻辑
+    if(isOutside.value) return
+    console.log('后续逻辑执行了') 
+
     //有效范围内控制滑块距离
     //横向
     if(elementX.value > 100 && elementX.value < 300) {
@@ -39,19 +47,25 @@ watch([elementX, elementY], () => {
 
     if(elementY.value > 300) {top.value = 200}
     if(elementY.value < 100) {top.value = 0}
+
+    //控制大图显示
+    positionX.value = -left.value * 2
+    positionY.value = -top.value * 2
 })
+
+
 </script>
 
 
 
 <template>
-{{elementX}}, {{elementY}}, {{isOutside}} {{top}}, {{left}}
+{{elementX}}, {{elementY}}, {{isOutside}} {{top}}, {{left}} {{positionX}}
   <div class="goods-image" ref="target">
     <!-- 左侧大图-->
     <div class="middle" ref="target">
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
+      <div class="layer" v-show="!isOutside" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
@@ -63,10 +77,10 @@ watch([elementX, elementY], () => {
     <div class="large" :style="[
       {
         backgroundImage: `url(${imageList[0]})`,
-        backgroundPositionX: `0px`,
-        backgroundPositionY: `0px`,
-      },
-    ]" v-show="false"></div>
+        backgroundPositionX: `${positionX}px`,
+        backgroundPositionY: `${positionY}px`,
+      }, 
+    ]" v-show="!isOutside"></div>
   </div>
 </template>
 
