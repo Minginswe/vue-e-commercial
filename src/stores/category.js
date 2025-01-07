8b03e5baf0f8ced583f02bc79cda26b7b580ebc7
Minginswe <xiaomingvlog@gmail.com>
2025-01-07 17:54:29 +0800

    pinia user info

--- src/stores/category.js
+++ src/stores/category.js
@@ -5,7 +5,7 @@ import {getCategoryAPI} from '@/apis/layout.js'
 
 export const useCategoryStore = defineStore('category', () => {
    
-
+  
   //导航列表数据
   const categoryList = ref([])
 
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -7,6 +7,9 @@ import 'element-plus/theme-chalk/el-message.css'
 
 import { useRouter } from 'vue-router'
 
+import {useUserStore} from '@/stores/user'
+const userStore = useUserStore()
+
 //表单校验（账户名+密码)
 
 //1.准备表单对象
@@ -51,8 +54,7 @@ const doLogin = () => {
         //以valid作为判断条件 如果通过校验才执行登录逻辑
         if(valid) {
             //TO DO Login
-            const res = await loginAPI({account, password})
-            console.log(res)
+            await userStore.getUserInfo({account, password})
             //1.提示用户
             ElMessage({type:'success', message:'登录成功'})
             //2.跳转首页
